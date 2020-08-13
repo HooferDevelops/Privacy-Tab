c39564fc442012e48d5bfd798b8bd2e72ed91d1e
HooferDevelops <60201971+HooferDevelops@users.noreply.github.com>
2020-08-13 02:57:15 -0400

    Add files via upload

---
+++ Settings.jsx
@@ -0,0 +1,70 @@
+const { React } = require('powercord/webpack')
+const { SwitchItem, TextInput, SliderInput, KeybindRecorder } = require('powercord/components/settings')
+const { Divider } = require('powercord/components')
+
+module.exports = class Settings extends React.PureComponent {
+    constructor (props) {
+        super(props);
+        this.plugin = powercord.pluginManager.get('privacy-tab');
+      }
+
+
+    render() {
+        return ( 
+        <div>
+        <KeybindRecorder
+            value={this.props.getSetting('toggle-keybind-value')}
+            defaultValue={'Ctrl+Shift+P'}
+            initialValue={this.props.getSetting('toggle-keybind-value', 'Ctrl+Shift+P')}
+            onChange={val => { 
+                this.plugin.updateKeybinds(this.props.getSetting('toggle-keybind-value'), val);
+                this.props.updateSetting('toggle-keybind-value', val); 
+            }}
+            onReset={()=>{
+                this.plugin.updateKeybinds(this.props.getSetting('toggle-keybind-value'), 'Ctrl+Shift+P');
+                this.props.updateSetting('toggle-keybind-value', 'Ctrl+Shift+P');
+            }}
+        >
+            Toggle Keybind
+        </KeybindRecorder>
+        <SwitchItem
+            value={ this.props.getSetting('global-keybind') }
+            onChange={ () => {
+                this.props.toggleSetting('global-keybind')
+            }}
+            note='If enabled, it will listen for keybinds outside of Discord.'
+        >
+            Global Keybind
+        </SwitchItem>
+        <br></br>
+        <SliderInput
+               minValue={0.5}
+               maxValue={10}
+               stickToMarkers
+               markers={[0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+               defaultValue={1}
+               initialValue={this.props.getSetting('blur-scale', 1)}
+               onValueChange={val => this.props.updateSetting('blur-scale', val)}
+               note='Scale of the blur amount.'
+               onMarkerRender={v => `x${v}`}
+        >
+            Blur Scale
+        </SliderInput>
+        <SwitchItem
+            value={ this.props.getSetting('grayscale') }
+            onChange={ () => {
+                this.props.toggleSetting('grayscale')
+            }}
+        >
+            Grayscale Enabled
+        </SwitchItem>
+        <a
+        style={{fontSize: "10px"}}
+        onClick={()=> {
+            require("electron").shell.openExternal("http://iman.engineer");
+        }}
+        >More Plugins & Themes</a>
+        </div>
+        );
+    }
+}
---
+++ index.js
@@ -0,0 +1,84 @@
+const { Plugin } = require('powercord/entities');
+const { React, getModule, FluxDispatcher, i18n: { Messages } } = require('powercord/webpack');
+const { description } = require('../pc-commands/commands/echo');
+const { Powercord } = require('../pc-updater/components/Icons');
+const { remote: { globalShortcut } } = require('electron');
+const Settings = require("./Settings")
+
+var privacyEnabled = false;
+var get;
+var set;
+var style;
+
+module.exports = class PrivacyTab extends Plugin {
+
+    startPlugin(){
+        get = this.settings.get;
+        set = this.settings.set;
+        // make sure it's safe, idk if needed, but why not?
+        if (!get("toggle-keybind-value"))
+            set("toggle-keybind-value", "Ctrl+Shift+P");
+        if (!get("global-keybind"))
+            set("global-keybind", true);
+        if (!get("blur-scale"))
+            set("blur-scale", 1);
+        if (!get("grayscale"))
+            set("grayscale", false);
+        
+        // init
+        globalShortcut.register(get("toggle-keybind-value"), this.togglePrivacy);
+
+        style = document.createElement('style');
+        document.head.appendChild(style);
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(``));
+
+        // register settings
+        powercord.api.settings.registerSettings(this.entityID, {
+            category: this.entityID,
+            label: 'Privacy Tab',
+            render: Settings
+        });
+    }
+
+    pluginWillUnload(){
+        globalShortcut.unregister(get("toggle-keybind-value"));
+        powercord.api.settings.unregisterSettings(this.entityID);
+        if (privacyEnabled){
+            togglePrivacy();
+        }
+        style.parentNode.removeChild(style);
+    }
+
+    // keybind update event, from settings
+    updateKeybinds(oldKeybind, newKeybind){
+        console.log(this.settings)
+        globalShortcut.unregister(oldKeybind)
+        globalShortcut.register(newKeybind, this.togglePrivacy)
+    }
+
+    // toggle for privacy
+    togglePrivacy(){
+        // check for global keybind, user safety #1 :P
+        if (!get("global-keybind") && !document.hasFocus()){
+            return;
+        }
+        
+        privacyEnabled = !privacyEnabled;
+        var blurElement = document.getElementsByClassName("app-1q1i1E")[0];
+
+        if (privacyEnabled){
+            // enable blur
+            var grayscale = "";
+            if (get("grayscale")){
+                grayscale = "grayscale(100%)"
+            }
+            blurElement.style = `transition: .3s linear; filter: blur(${get("blur-scale")*3}px) ${grayscale};`;
+        } else {
+            // disable blur, durr
+            blurElement.style = `transition: .3s linear;`;
+        }
+
+    }
+
+};
